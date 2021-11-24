2d39ce34e762ec0b95564d0032e8eaa98d19b8a7
Marie Fischer <marie.fischer@student.hpi.uni-potsdam.de>
2021-11-24 14:59:33 +0100

    add basic components, add basic router, add boostrap for css

---
+++ frontend/src/app/pages/database-selection/database-selection.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-database-selection',
+  templateUrl: './database-selection.component.html',
+  styleUrls: ['./database-selection.component.css']
+})
+export class DatabaseSelectionComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+}
---
+++ frontend/src/app/pages/normalize/normalize.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { NormalizeComponent } from './normalize.component';
+
+describe('NormalizeComponent', () => {
+  let component: NormalizeComponent;
+  let fixture: ComponentFixture<NormalizeComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ NormalizeComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(NormalizeComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
