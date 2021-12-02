6b2f4592721e1f4d8b7d026a36279e31c7c8f736
j-adamcic <janina.adamcic@gmail.com>
2021-12-02 15:12:01 +0100

    side bar component tests

--- frontend/src/app/components/normalize-side-bar/normalize-side-bar.component.spec.ts
+++ frontend/src/app/components/normalize-side-bar/normalize-side-bar.component.spec.ts
@@ -2,6 +2,8 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { NormalizeSideBarComponent } from './normalize-side-bar.component';
 
+import { exampleTable } from 'src/model/schema/exampleTables';
+
 describe('NormalizeSideBarComponent', () => {
   let component: NormalizeSideBarComponent;
   let fixture: ComponentFixture<NormalizeSideBarComponent>;
@@ -15,10 +17,37 @@ describe('NormalizeSideBarComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(NormalizeSideBarComponent);
     component = fixture.componentInstance;
+    component.table = exampleTable();
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should have table name as headline', () => {
+    const sideBarElement: HTMLElement = fixture.nativeElement;
+    expect(sideBarElement.innerHTML).toContain(exampleTable().name);
+  });
+
+  it('should display all keys', () => {
+    const sideBarElement: HTMLElement = fixture.nativeElement;
+    exampleTable()
+      .keys()
+      .forEach((key) => {
+        expect(sideBarElement.innerHTML).toContain(key.toString());
+      });
+  });
+
+  it('should display all violating fds', () => {
+    const sideBarElement: HTMLElement = fixture.nativeElement;
+    console.log(sideBarElement.innerHTML);
+    exampleTable()
+      .violatingFds()
+      .forEach((fd) => {
+        expect(sideBarElement.innerHTML).toContain(
+          fd.toString().replace('>', '&gt;')
+        );
+      });
   });
 });
