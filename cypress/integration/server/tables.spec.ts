18fa507ce498d32815f6e154de203c21c21369a1
Paul Sieben <67124476+PaulVII@users.noreply.github.com>
2021-12-02 18:42:09 +0100

    add typeguards, make nicer initial tests

--- cypress/integration/server/tables.spec.ts
+++ cypress/integration/server/tables.spec.ts
@@ -1,5 +1,4 @@
-import { is } from "typerscript-is";
-import ITable from "../server/definitions/ITable";
+import { isITable } from "../../../server/definitions/ITable.guard";
 
 describe("The /tables route", () => {
   // see https://www.cypress.io/blog/2017/11/07/add-gui-to-your-e2e-api-tests/
@@ -12,7 +11,11 @@ describe("The /tables route", () => {
   });
   it("Should return tables", () => {
     cy.request("get", Cypress.env("serverUrl") + "/tables").should((result) => {
-      cy.log(result.body);
+      // reference for assertions: https://docs.cypress.io/guides/references/assertions#BDD-Assertions
+
+      expect(result.body).to.be.an("array");
+      for (const table of result.body)
+        expect(isITable(table), "type guard check").to.be.true;
     });
   });
 });
