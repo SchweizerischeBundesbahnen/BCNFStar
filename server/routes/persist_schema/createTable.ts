0d40836d8182f060c42ce2158f91db2057d27f67
Chris <chriski99@web.de>
2021-12-09 14:08:31 +0100

    SQL injection safe, refactoring to databaseUtils

--- server/routes/persist_schema/createTable.ts
+++ server/routes/persist_schema/createTable.ts
@@ -1,55 +1,124 @@
-import ITableHead from "@/definitions/ITableHead";
 import { Request, Response, RequestHandler } from "express";
-import { Pool } from "pg";
-import { EOL } from "os";
+import { Pool, PoolClient } from "pg";
+import {
+  tableExistsInSchema,
+  schemaExistsInDatabase,
+  attributesExistInTable,
+} from "../../utils/databaseUtils";
 
-export default function postCreateTable(
-    pool: Pool
-): RequestHandler {
-    async function createTable(
-        req: Request,
-        res: Response
-    ): Promise<void> {
-        try {
-            const body = req.body;
-            let schema = body.schema;
-            const table = body.name;
-            const attributes = body.attribute;
+async function prepareDatabase(
+  client: PoolClient,
+  newSchema: string,
+  newTable: string
+) {
+  await client.query(`CREATE SCHEMA IF NOT EXISTS ${newSchema}`);
+  await client.query(`DROP TABLE IF EXISTS ${newSchema}.${newTable};`);
+}
 
-            
-            const client = await pool.connect();
+function isInvalidName(name: string): boolean {
+  return !/^[a-zA-Z\_]+$/.test(name);
+}
 
-            schema = `ABC; DROP TABLE testschema.test;`;
+function parseBody(body: any): string[][] {
+  const attributeNames: string[] = body.attribute.map((a) => a.name);
+  const primaryKey: string[] = body.primaryKey;
+  const newSchema: string = body.newSchema;
+  const newTable: string = body.newTable;
+  const originSchema: string = body.originSchema;
+  const originTable: string = body.originTable;
 
+  return [
+    attributeNames,
+    primaryKey,
+    [newSchema, newTable, originSchema, originTable],
+  ];
+}
 
-            
-            // parameter = "' DROP TABLE ... -- ";
-/*
-SELECT ..... 
-FROM .... 
-WHERE a = '${parameter}'
+async function executeCreateTable(
+  client: PoolClient,
+  attributeNames: string[],
+  originSchema: string,
+  originTable: string,
+  newSchema: string,
+  newTable: string
+): Promise<void> {
+  await client.query(`CREATE TABLE ${newSchema}.${newTable} AS SELECT
+        ${attributeNames
+          .map((a) => '"' + a + '"')
+          .join(", ")} FROM ${originSchema}.${originTable};`);
+}
 
-SELECT ..... 
-FROM .... 
-WHERE a = '' DROP TABLE ... -- '
+async function addPrimaryKey(
+  client: PoolClient,
+  newSchema: string,
+  newTable: string,
+  primaryKey: string[]
+): Promise<void> {
+  await client.query(
+    `ALTER TABLE ${newSchema}.${newTable} ADD PRIMARY KEY (${primaryKey
+      .map((a) => '"' + a + '"')
+      .join(", ")});`
+  );
+}
 
+export default function postCreateTable(pool: Pool): RequestHandler {
+  async function createTable(req: Request, res: Response): Promise<void> {
+    try {
+      const [
+        attributeNames,
+        primaryKey,
+        [newSchema, newTable, originSchema, originTable],
+      ] = parseBody(req.body);
 
-*/
-            await client.query(`CREATE SCHEMA IF NOT EXISTS ${schema}`);
-            // await client.query(`DROP TABLE IF EXISTS ${schema}.${table};`);
-            // console.log(attributes.map(a => a.name + " " + a.dataType).join("," + EOL));
-            // await client.query(`CREATE TABLE ${schema}.${table} (
-            //     ${attributes.map(a => a.name + " " + a.dataType).join(",")}
-            //     );`);
-            console.log("schema: ", schema);
-            console.log("table: ", table);
-            console.log("attributes: ", attributes);
-        } catch (error) {
-            console.error(error);
-            res.json({ error: "Could not get tables" });
-            res.status(502);
+      if (isInvalidName(newSchema) || isInvalidName(newTable)) {
+        res.json("invalid characters in new schema/table name");
+        res.status(400);
+        return;
+      }
+
+      const client = await pool.connect();
+
+      if (
+        !tableExistsInSchema(client, originSchema, originTable) ||
+        !schemaExistsInDatabase(client, originSchema)
+      ) {
+        res.json("please type in a schema/table that exists");
+        res.status(400);
+        return;
+      }
+
+      console.log(primaryKey);
+      prepareDatabase(client, newSchema, newTable).then(async () => {
+        if (
+          attributesExistInTable(
+            client,
+            attributeNames,
+            originSchema,
+            originTable
+          )
+        ) {
+          executeCreateTable(
+            client,
+            attributeNames,
+            originSchema,
+            originTable,
+            newSchema,
+            newTable
+          ).then(async () => {
+            await addPrimaryKey(client, newSchema, newTable, primaryKey);
+          });
+          res.json("successfull");
+          res.status(200);
+        } else {
+          res.json("attributes dont exist");
+          res.status(400);
         }
+      });
+    } catch (error) {
+      console.error(error);
+      res.json({ error: "Could not create tables" });
+      res.status(502);
     }
-
-    return createTable;
+  }
+  return createTable;
 }
