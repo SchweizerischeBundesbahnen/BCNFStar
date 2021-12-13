02c5bda889df352d6281d6cbbbd835239ee543e9
Paul Sieben <67124476+PaulVII@users.noreply.github.com>
2021-12-13 16:09:50 +0100

    align frontend and backend port setting

--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -19,4 +19,6 @@
 module.exports = (on, config) => {
   // `on` is used to hook into various events Cypress emits
   // `config` is the resolved Cypress config
+  if (!process.env.CYPRESS_BACKEND_BASEURL)
+    process.env.CYPRESS_BACKEND_BASEURL = "http://localhost:80";
 };
