01bfb4334214040c0e6a65c8abab27227439c088
Tobias Sträubig <tobi_2001@hotmail.de>
2021-12-14 20:06:53 +0100

    fixed table-selection component test

--- frontend/src/app/pages/table-selection/table-selection.component.spec.ts
+++ frontend/src/app/pages/table-selection/table-selection.component.spec.ts
@@ -1,37 +1,30 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-import {
-  HttpClientTestingModule,
-  HttpTestingController,
-} from '@angular/common/http/testing';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 
 import { TableSelectionComponent } from './table-selection.component';
 
 import { DatabaseService } from 'src/app/database.service';
 import { exampleITable } from 'src/model/schema/exampleTables';
-import { Type } from '@angular/core';
+import { of } from 'rxjs';
 
 describe('TableSelectionComponent', () => {
   let component: TableSelectionComponent;
   let fixture: ComponentFixture<TableSelectionComponent>;
-  let httpMock: HttpTestingController;
+  let databaseServiceStub: any;
 
   beforeEach(async () => {
+    databaseServiceStub = {
+      getTableNames: () => of(exampleITable),
+    };
     await TestBed.configureTestingModule({
       declarations: [TableSelectionComponent],
       imports: [HttpClientTestingModule],
-      providers: [{ provide: DatabaseService }],
+      providers: [{ provide: DatabaseService, useValue: databaseServiceStub }],
     }).compileComponents();
 
     fixture = TestBed.createComponent(TableSelectionComponent);
     component = fixture.componentInstance;
-    httpMock = fixture.debugElement.injector.get<HttpTestingController>(
-      HttpTestingController as Type<HttpTestingController>
-    );
     fixture.detectChanges();
-  });
-
-  afterEach(() => {
-    httpMock.verify();
   });
 
   it('should create', () => {
@@ -40,12 +33,8 @@ describe('TableSelectionComponent', () => {
 
   it('should display all tables', () => {
     const tableSelectionElement: HTMLElement = fixture.nativeElement;
-    const req = httpMock.expectOne(
-      `http://${window.location.hostname}:80/tables`
-    );
-    expect(req.request.method).toBe('GET');
-    req.flush(exampleITable);
-    expect(fixture.componentInstance.tables.length).toBe(exampleITable.length);
+
+    expect(component.tables).toEqual(exampleITable);
     exampleITable.forEach((table) => {
       expect(tableSelectionElement.innerHTML).toContain(table.name);
     });
