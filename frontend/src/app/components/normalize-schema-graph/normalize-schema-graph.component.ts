310b51f28b12fc7f0cf93a4a39f93609004b843f
Paul Sieben <67124476+PaulVII@users.noreply.github.com>
2022-01-06 14:21:12 +0100

    add zoom

--- frontend/src/app/components/normalize-schema-graph/normalize-schema-graph.component.ts
+++ frontend/src/app/components/normalize-schema-graph/normalize-schema-graph.component.ts
@@ -13,6 +13,7 @@ import * as joint from 'jointjs';
 import Table from 'src/model/schema/Table';
 import * as dagre from 'dagre';
 import * as graphlib from 'graphlib';
+import panzoom, { PanZoom, Transform } from 'panzoom';
 
 type GraphStorageItem = {
   jointjsEl: joint.dia.Element;
@@ -36,20 +37,29 @@ export class NormalizeSchemaGraphComponent implements OnChanges, AfterViewInit {
     if (changes['tables']) this.createDefaultGraph();
   }
 
+  private panzoomTransform!: Transform;
   ngAfterViewInit(): void {
     setTimeout(() => this.updateAllBBoxes(), 100);
+    const panzoomHandler = panzoom(document.querySelector('svg') as SVGElement);
+    // move all HTML overlays whenever the user zoomed or panned
+    panzoomHandler.on('transform', (e: PanZoom) => {
+      this.panzoomTransform = e.getTransform();
+      this.updateAllBBoxes();
+    });
   }
 
   protected attributeWidth = 22.5;
 
   public graphStorage: Record<string, GraphStorageItem> = {};
 
-  createDefaultGraph() {
-    let graph = new joint.dia.Graph();
+  protected graph!: joint.dia.Graph;
 
-    let paper = new joint.dia.Paper({
+  createDefaultGraph() {
+    this.graph = new joint.dia.Graph();
+
+    new joint.dia.Paper({
       el: document.getElementById('paper') || undefined,
-      model: graph,
+      model: this.graph,
       height: 1000,
       width: '100%',
       gridSize: 1,
@@ -58,7 +68,8 @@ export class NormalizeSchemaGraphComponent implements OnChanges, AfterViewInit {
         color: 'rgba(200, 200, 200, 0.3)',
       },
     });
-    graph.on('change:position', (element) => {
+    // move the corresponding HTML overlay whenever a graph element changes position
+    this.graph.on('change:position', (element) => {
       if (element.isElement)
         for (const item of Object.values(this.graphStorage)) {
           if (item.jointjsEl == element) {
@@ -67,7 +78,24 @@ export class NormalizeSchemaGraphComponent implements OnChanges, AfterViewInit {
         }
     });
 
-    // generate elements
+    this.generateElements();
+    this.generateLinks();
+    joint.layout.DirectedGraph.layout(this.graph, {
+      dagre,
+      graphlib,
+      nodeSep: 40,
+      edgeSep: 80,
+      rankDir: 'LR',
+    });
+  }
+  @ViewChild('paper') paperHtmlObject!: ElementRef<HTMLDivElement>;
+  get paperOffset() {
+    const { top, left } =
+      this.paperHtmlObject.nativeElement.getBoundingClientRect();
+    return { top: top + window.scrollY, left: left + window.scrollX };
+  }
+
+  generateElements() {
     for (const table of this.tables) {
       const jointjsEl = new joint.shapes.standard.Rectangle({
         attrs: { root: { id: '__jointel__' + table.name } },
@@ -83,39 +111,6 @@ export class NormalizeSchemaGraphComponent implements OnChanges, AfterViewInit {
         60 + this.attributeWidth * table.columns.columns.size
       );
       jointjsEl.position(50, 10);
-      let counter = 0;
-      for (let column of table.columns.inOrder()) {
-        jointjsEl.addPort({
-          id: column.name + '_right', // generated if `id` value is not present
-          group: 'ports-right',
-          args: {}, // extra arguments for the port layout function, see `layout.Port` section
-          label: {
-            position: {
-              name: 'right',
-              // args: { y: 60 + 22.5 * i } // extra arguments for the label layout function, see `layout.PortLabel` section
-            },
-          },
-          markup: `<circle r="${this.attributeWidth / 2}" cx="0" cy="${
-            25 + this.attributeWidth / 2 + this.attributeWidth * counter
-          }" strokegit ="green" fill="white" />`,
-        });
-        jointjsEl.addPort({
-          id: column.name + '_left', // generated if `id` value is not present
-          group: 'ports-left',
-          args: {}, // extra arguments for the port layout function, see `layout.Port` section
-          label: {
-            position: {
-              name: 'left',
-              // args: { y: 60 + 22.5 * i } // extra arguments for the label layout function, see `layout.PortLabel` section
-            },
-          },
-          markup: `<circle r="${this.attributeWidth / 2}" cx="300" cy="${
-            25 + this.attributeWidth / 2 + this.attributeWidth * counter
-          }" strokegit ="red" fill="white" />`,
-        });
-        counter++;
-      }
-
       this.graphStorage[table.name] = {
         // alternative to HtmlElement: joint.shapes.html.Element
         jointjsEl,
@@ -123,10 +118,12 @@ export class NormalizeSchemaGraphComponent implements OnChanges, AfterViewInit {
         table,
         links: {},
       };
-      graph.addCell(jointjsEl);
+      this.generatePorts(jointjsEl, table);
+      this.graph.addCell(jointjsEl);
     }
+  }
 
-    // generate links
+  generateLinks() {
     for (const table of this.tables) {
       for (const otherTable of table.minimalReferencedTables()) {
         let foreignKey = table
@@ -145,31 +142,43 @@ export class NormalizeSchemaGraphComponent implements OnChanges, AfterViewInit {
             },
           });
       }
-      graph.addCells(Object.values(this.graphStorage[table.name].links));
+      this.graph.addCells(Object.values(this.graphStorage[table.name].links));
     }
-    joint.layout.DirectedGraph.layout(graph, {
-      dagre,
-      graphlib,
-      nodeSep: 40,
-      edgeSep: 80,
-      rankDir: 'LR',
-    });
-
-    // var link = new joint.dia.Link({
-    //   source: { id: rect.id },
-    //   target: { id: rect2.id },
-    // });
-
-    paper.on('element:pointerup', function () {
-      // alert('Hello')
-    });
-    // graph.addCells([rect, rect2, link]);
   }
-  @ViewChild('paper') paperHtmlObject!: ElementRef<HTMLDivElement>;
-  get paperOffset() {
-    const { top, left } =
-      this.paperHtmlObject.nativeElement.getBoundingClientRect();
-    return { top: top + window.scrollY, left: left + window.scrollX };
+
+  generatePorts(jointjsEl: joint.dia.Element, table: Table) {
+    let counter = 0;
+    for (let column of table.columns.inOrder()) {
+      jointjsEl.addPort({
+        id: column.name + '_right', // generated if `id` value is not present
+        group: 'ports-right',
+        args: {}, // extra arguments for the port layout function, see `layout.Port` section
+        label: {
+          position: {
+            name: 'right',
+            // args: { y: 60 + 22.5 * i } // extra arguments for the label layout function, see `layout.PortLabel` section
+          },
+        },
+        markup: `<circle r="${this.attributeWidth / 2}" cx="0" cy="${
+          25 + this.attributeWidth / 2 + this.attributeWidth * counter
+        }" strokegit ="green" fill="white" />`,
+      });
+      jointjsEl.addPort({
+        id: column.name + '_left', // generated if `id` value is not present
+        group: 'ports-left',
+        args: {}, // extra arguments for the port layout function, see `layout.Port` section
+        label: {
+          position: {
+            name: 'left',
+            // args: { y: 60 + 22.5 * i } // extra arguments for the label layout function, see `layout.PortLabel` section
+          },
+        },
+        markup: `<circle r="${this.attributeWidth / 2}" cx="300" cy="${
+          25 + this.attributeWidth / 2 + this.attributeWidth * counter
+        }" strokegit ="red" fill="white" />`,
+      });
+      counter++;
+    }
   }
 
   updateAllBBoxes() {
@@ -183,52 +192,13 @@ export class NormalizeSchemaGraphComponent implements OnChanges, AfterViewInit {
     item.style = {
       width: bbox.width + 'px',
       height: bbox.height + 'px',
-      left: bbox.x + this.paperOffset.left + 'px',
-      top: bbox.y + this.paperOffset.top + 'px ',
-      // transform: 'rotate(' + (this.get('angle') || 0) + 'deg)'
+      left: bbox.x + this.paperOffset.left + this.panzoomTransform.x + 'px',
+      top: bbox.y + this.paperOffset.top + this.panzoomTransform.y + 'px ',
+      transform: `scale(${this.panzoomTransform.scale})`,
+      'transform-origin': 'left top',
     };
 
     const domel = document.getElementById('__jointel__' + item.table.name);
     domel?.setAttribute('transform', `translate(${bbox.x}, ${bbox.y})`);
-  }
-
-  createTableBox(
-    graph: joint.dia.Graph<
-      joint.dia.Graph.Attributes,
-      joint.dia.ModelSetOptions
-    >,
-    numAttr: number = 1
-  ) {
-    let rectangle = new joint.shapes.standard.HeaderedRectangle();
-    rectangle.resize(100, 100);
-    rectangle.position(50, 10);
-    rectangle.attr({
-      headerText: {
-        text: 'Header',
-      },
-      bodyText: {
-        text: 'test',
-      },
-    });
-    rectangle.addTo(graph);
-    // graph.addCells([rectangle]);
-    let attribute;
-    for (let i = 0; i < numAttr; i++) {
-      attribute = new joint.shapes.devs.Atomic();
-      attribute.resize(100, 20);
-      attribute.position(50, 20 * (i + 2));
-      attribute.attr({
-        label: {
-          text: 'Attribute' + (i + 1).toString(),
-        },
-        body: {
-          fill: 'white',
-        },
-      });
-      rectangle.embed(attribute);
-      // graph.addCells([attribute]);
-      attribute.addTo(graph);
-    }
-    return rectangle;
   }
 }
