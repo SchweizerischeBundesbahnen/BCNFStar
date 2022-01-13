87696a97e413150ab7b9e46ff8156391ac914c42
j-adamcic <janina.adamcic@gmail.com>
2022-01-13 16:04:05 +0100

    fd extension

--- frontend/src/model/schema/Schema.ts
+++ frontend/src/model/schema/Schema.ts
@@ -1,3 +1,4 @@
+import ColumnCombination from './ColumnCombination';
 import FunctionalDependency from './FunctionalDependency';
 import Table from './Table';
 
@@ -58,21 +59,54 @@ export default class Schema {
 
   public join(table1: Table, table2: Table) {
     let newTable = table1.join(table2);
-    this.setFdsFor(newTable);
+    this.setFdsFor(newTable, table1, table2);
     this.add(newTable);
     this.delete(table1);
     this.delete(table2);
     return newTable;
   }
 
-  private setFdsFor(table: Table) {
-    let sourceTables = new Set<Table>();
+  private setFdsFor(table: Table, parent1: Table, parent2: Table) {
+    /*let sourceTables = new Set<Table>();
     table.columns.columns.forEach((column) => {
       sourceTables.add(column.sourceTable);
     });
 
     sourceTables.forEach((sourceTable) => {
       sourceTable.projectFds(table);
+    });*/
+    parent1.projectFds(table);
+    parent2.projectFds(table);
+
+    // extension
+    let fk = parent1.columns.copy().intersect(parent2.columns);
+    let fkFds1 = new Map<ColumnCombination, ColumnCombination>(); // fds from parent1 where lhs isSubsetOf fk
+    let fkFds2 = new Map<ColumnCombination, ColumnCombination>(); // fds from parent2 where lhs isSubsetOf fk
+
+    parent1.fds.forEach((fd) => {
+      if (fd.lhs.isSubsetOf(fk)) fkFds1.set(fd.lhs, fd.rhs);
     });
+    parent2.fds.forEach((fd) => {
+      if (fd.lhs.isSubsetOf(fk)) fkFds2.set(fd.lhs, fd.rhs);
+    });
+
+    parent1.fds.forEach((fd) => {
+      let rhsFkPart = fd.rhs.copy().intersect(fk);
+      if (fkFds2.has(rhsFkPart)) {
+        table.fds
+          .filter((fd1) => fd1 == fd)[0]
+          .rhs.union(fkFds2.get(rhsFkPart)!);
+      }
+    });
+    parent2.fds.forEach((fd) => {
+      let rhsFkPart = fd.rhs.copy().intersect(fk);
+      if (fkFds1.has(rhsFkPart)) {
+        table.fds
+          .filter((fd1) => fd1 == fd)[0]
+          .rhs.union(fkFds1.get(rhsFkPart)!);
+      }
+    });
+
+    table.setFds(...new Set(table.fds)); //remove duplicate fds with lhs = fk subset
   }
 }
