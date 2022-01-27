6de895e7e8458d7a4ab1b3c3b89875366e5646d0
rothaarlappen <37540536+rothaarlappen@users.noreply.github.com>
2022-01-27 12:13:58 +0100

    refactoring: ind-route starts algorithm, if outputfile does not exist

--- server/routes/indsForTables.ts
+++ server/routes/indsForTables.ts
@@ -5,18 +5,34 @@ import { split } from "../utils/databaseUtils";
 import IInclusionDependency, {
   IColumnIdentifier,
 } from "../definitions/IInclusionDependencies";
+import { runMetanomeINDAlgorithm } from "./runMetanomeIND";
 
 export default function getINDsForTablesFunction(): RequestHandler {
   async function getINDsForTables(req: Request, res: Response): Promise<void> {
     try {
       const tables: string[] = req.params.tableNames.split(",");
-
       const expectedOutputPath: string =
         MetanomeINDAlgorithm.outputPath(tables);
+      try {
+        await sendINDs(expectedOutputPath);
+      } catch (err) {
+        // means file not found
+        if (err.code === "ENOENT") {
+          await runMetanomeINDAlgorithm(tables);
+          await sendINDs(expectedOutputPath);
+        } else {
+          throw err;
+        }
+      }
+    } catch (err) {
+      if (!res.headersSent)
+        res.status(502).json({ error: "Could not get inds for table... " });
+    }
+
+    async function sendINDs(expectedOutputPath: string) {
       const inds: IInclusionDependency[] = await readINDsFromFile(
         expectedOutputPath
       );
-
       inds.forEach((ind) => {
         ind.dependant.columnIdentifiers.forEach((column) =>
           splitTableIdentifier(column)
@@ -25,15 +41,11 @@ export default function getINDsForTablesFunction(): RequestHandler {
           splitTableIdentifier(column)
         );
       });
-
       res.json(inds);
       res.status(200);
-    } catch (error) {
-      console.error(error);
-      if (!res.headersSent)
-        res.status(502).json({ error: "Could not get inds for table... " });
     }
   }
+
   return getINDsForTables;
 }
 
