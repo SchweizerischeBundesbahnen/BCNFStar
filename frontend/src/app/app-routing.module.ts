bbf48766f6877417f409557f4bf950bb874e6d2c
Paul Sieben <67124476+PaulVII@users.noreply.github.com>
2022-03-15 18:34:33 +0100

    remove daabase selection

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,14 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { TableSelectionComponent } from './pages/table-selection/table-selection.component';
-import { DatabaseSelectionComponent } from './pages/database-selection/database-selection.component';
 import { NormalizeComponent } from './pages/normalize/normalize.component';
 
 const routes: Routes = [
-  { path: '', component: DatabaseSelectionComponent },
-  { path: 'table-selection', component: TableSelectionComponent },
+  { path: '', component: TableSelectionComponent },
   { path: 'normalize', component: NormalizeComponent },
-  { path: '**', redirectTo: '/database-selection' },
+  { path: '**', redirectTo: '' },
 ];
 
 @NgModule({
