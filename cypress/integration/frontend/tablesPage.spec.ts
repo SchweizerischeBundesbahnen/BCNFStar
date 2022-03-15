fb8fd9ae725f33bb8f07f7c7153bda739962183d
Paul Sieben <67124476+PaulVII@users.noreply.github.com>
2022-03-15 18:44:55 +0100

    fix test

--- cypress/integration/frontend/tablesPage.spec.ts
+++ cypress/integration/frontend/tablesPage.spec.ts
@@ -3,10 +3,8 @@
 describe("The tables page", () => {
   beforeEach(() => {
     cy.visitFrontend();
-    cy.contains("Default Connection").click();
   });
   it("should be able to navigate to the normalize page", () => {
-    cy.url().should("contain", "/table-selection");
     // contains asserts that a element with the content exists and selects it
     cy.contains("public.nation_region_denormalized").click();
     cy.contains("Go").click();
