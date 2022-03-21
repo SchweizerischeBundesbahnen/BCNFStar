9f1df3decea34e05c4025c5a97d6a2f2e4c6124e
Paul Sieben <67124476+PaulVII@users.noreply.github.com>
2022-03-21 16:59:11 +0100

    don't return nothing when just a single tableHead had an error

--- server/routes/rowCounts.ts
+++ server/routes/rowCounts.ts
@@ -5,23 +5,24 @@ export default function getTableRowCountsFunction(): RequestHandler {
   async function getTableRowCounts(req: Request, res: Response): Promise<void> {
     try {
       const query_result_tables = await sqlUtils.getSchema();
-      let temp_table_rowCounts: Record<string, Promise<number>> = {};
+      let rowCounts: Record<string, Promise<number | void>> = {};
 
       for (const row of query_result_tables) {
         const complete_name = `${row.table_schema}.${row.table_name}`;
-        if (!temp_table_rowCounts[complete_name]) {
-          temp_table_rowCounts[complete_name] = sqlUtils.getTableRowCount(
-            row.table_name,
-            row.table_schema
-          );
+        if (!rowCounts[complete_name]) {
+          rowCounts[complete_name] = sqlUtils
+            .getTableRowCount(row.table_name, row.table_schema)
+            .catch((e) =>
+              console.error(
+                "Error while getting row count for table: " + complete_name
+              )
+            );
         }
       }
 
-      const values = await Promise.all(Object.values(temp_table_rowCounts));
-      const keys = Object.keys(temp_table_rowCounts);
       const result: Record<string, number> = {};
-      for (const index in keys) {
-        result[keys[index]] = values[index];
+      for (const tablename in rowCounts) {
+        result[tablename] = (await rowCounts[tablename]) || 0;
       }
       res.json(result);
     } catch (error) {
