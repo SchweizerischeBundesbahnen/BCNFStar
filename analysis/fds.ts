c7b646eb3b3620aaf96f7ea114e452685b618b5e
Paul Sieben <67124476+PaulVII@users.noreply.github.com>
2022-03-24 12:49:41 +0100

    add a little script to analyse fd files

---
+++ analysis/fds.ts
@@ -0,0 +1,35 @@
+import { readFile } from "fs/promises";
+
+// give filename as first argument
+(async function () {
+  for (const filename of process.argv.slice(2)) {
+    console.log(filename);
+    await calculateFdStatistics(filename);
+  }
+})();
+
+async function calculateFdStatistics(filename: string) {
+  const lhsCounts = {};
+  const rhsCounts = {};
+  const fds: { lhs: string[]; rhs: string[] }[] = [];
+  const fileContent = await readFile(filename, { encoding: "utf-8" });
+  for (const fdString of fileContent.split("\r\n")) {
+    if (!fdString) continue;
+    let [lhsString, rhsString] = fdString.split("] --> ");
+    lhsString = lhsString.substring(1);
+    const lhColumns = lhsString.split(", ");
+    const rhColumns = rhsString.split(", ");
+    fds.push({ lhs: lhColumns, rhs: rhColumns });
+    lhsCounts[lhColumns.length] = lhsCounts[lhColumns.length]
+      ? lhsCounts[lhColumns.length] + 1
+      : 1;
+    rhsCounts[rhColumns.length] = rhsCounts[rhColumns.length]
+      ? rhsCounts[rhColumns.length] + 1
+      : 1;
+  }
+  console.log("rh");
+  console.table(rhsCounts);
+  console.log("lh");
+  console.table(lhsCounts);
+  // console.log(fds.filter(v => v.rhs.length == 31))
+}
