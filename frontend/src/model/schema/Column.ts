118caec43c533c40e930bd5a57ce9ea028506847
rothaarlappen <37540536+rothaarlappen@users.noreply.github.com>
2022-04-05 17:14:22 +0200

    adds nullable-attribute to model-classes

--- frontend/src/model/schema/Column.ts
+++ frontend/src/model/schema/Column.ts
@@ -6,17 +6,20 @@ export default class Column {
   name: string;
   dataType: string;
   ordinalPosition: number;
+  nullable: boolean;
   source: ColumnIdentifier;
 
   public constructor(
     name: string,
     dataType: string,
     ordinalPosition: number,
+    nullable: boolean,
     source: ColumnIdentifier
   ) {
     this.name = name;
     this.dataType = dataType;
     this.ordinalPosition = ordinalPosition;
+    this.nullable = nullable;
     this.source = source;
   }
 
@@ -25,6 +28,7 @@ export default class Column {
       this.name,
       this.dataType,
       this.ordinalPosition,
+      this.nullable,
       this.source
     );
   }
@@ -38,6 +42,7 @@ export default class Column {
       name: this.name,
       table: this.source.table.name,
       dataType: this.dataType,
+      nullable: this.nullable,
     };
   }
 }
