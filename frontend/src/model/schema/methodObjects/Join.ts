2683980e3c47675c8ea5e107ab09c36e02c8f314
Tobias Sträubig <tobi_2001@hotmail.de>
2022-04-28 13:45:37 +0200

    first draft of new join algorithm

--- frontend/src/model/schema/methodObjects/Join.ts
+++ frontend/src/model/schema/methodObjects/Join.ts
@@ -35,12 +35,102 @@ export default class Join {
     this.referencing.sources.forEach((sourceTable) =>
       this.newTable.sources.push(sourceTable)
     );
+    this.newTable.relationships.push(...this.referencing.relationships);
 
     const sourceMapping = new Map<SourceTableInstance, SourceTableInstance>();
-    this.referenced.sources.forEach((source) => {
-      const newSource = this.newTable.addSource(source.table, this.name);
-      sourceMapping.set(source, newSource);
-    });
+    if (this.relationship.sourceRelationship().isTrivial) {
+      sourceMapping.set(
+        new ColumnCombination(
+          this.relationship.referenced
+        ).sourceTableInstance(),
+        new ColumnCombination(
+          this.relationship.referencing
+        ).sourceTableInstance()
+      );
+      for (const source of this.referenced
+        .sourcesTopological()
+        .reverse()
+        .slice(1)) {
+        const relToSource = this.referenced.relationships.find(
+          (relationship) =>
+            relationship.referenced[0].sourceTableInstance == source
+        )!;
+        const equivalentReferencingColumns = new ColumnCombination(
+          relToSource.referencing.map((column) =>
+            column.applySourceMapping(sourceMapping)
+          )
+        );
+        const equivalentRelationship = this.referencing.relationships.find(
+          (rel) =>
+            new ColumnCombination(rel.referencing).equals(
+              equivalentReferencingColumns
+            ) && rel.referenced[0].sourceTableInstance.table == source.table
+        );
+        if (equivalentRelationship) {
+          const equivalentSource =
+            equivalentRelationship.referenced[0].sourceTableInstance;
+          sourceMapping.set(source, equivalentSource);
+        } else {
+          const newSource = this.newTable.addSource(source.table, this.name);
+          sourceMapping.set(source, newSource);
+          // ????
+          this.newTable.relationships.push(
+            new Relationship(
+              relToSource.referencing.map((column) =>
+                column.applySourceMapping(sourceMapping)
+              ),
+              relToSource.referenced.map((column) =>
+                column.applySourceMapping(sourceMapping)
+              )
+            )
+          );
+        }
+      }
+    } else {
+      // ????
+      this.newTable.relationships.push(
+        ...this.referenced.relationships.map(
+          (rel) =>
+            new Relationship(
+              rel.referencing.map((column) =>
+                column.applySourceMapping(sourceMapping)
+              ),
+              rel.referenced.map((column) =>
+                column.applySourceMapping(sourceMapping)
+              )
+            )
+        )
+      );
+    }
+
+    // this.referenced.sources.forEach((source) => {
+    //   const newSource = this.newTable.addSource(source.table, this.name);
+    //   sourceMapping.set(source, newSource);
+    // });
+
+    // // relationships
+    // this.newTable.relationships.push(...this.referencing.relationships);
+    // this.newTable.relationships.push(
+    //   ...this.referenced.relationships.map(
+    //     (rel) =>
+    //       new Relationship(
+    //         rel.referencing.map((column) =>
+    //           column.applySourceMapping(sourceMapping)
+    //         ),
+    //         rel.referenced.map((column) =>
+    //           column.applySourceMapping(sourceMapping)
+    //         )
+    //       )
+    //   )
+    // );
+    // this.newTable.relationships.push(
+    //   new Relationship(
+    //     this.relationship.referencing,
+    //     this.relationship.referenced.map((column) =>
+    //       column.applySourceMapping(sourceMapping)
+    //     )
+    //   )
+    // );
 
     // columns
     this.newTable.columns.add(...this.referencing.columns);
@@ -49,30 +139,6 @@ export default class Join {
         .copy()
         .setMinus(new ColumnCombination(this.relationship.referenced))
         .applySourceMapping(sourceMapping)
-    );
-
-    // relationships
-    this.newTable.relationships.push(...this.referencing.relationships);
-    this.newTable.relationships.push(
-      ...this.referenced.relationships.map(
-        (rel) =>
-          new Relationship(
-            rel.referencing.map((column) =>
-              column.applySourceMapping(sourceMapping)
-            ),
-            rel.referenced.map((column) =>
-              column.applySourceMapping(sourceMapping)
-            )
-          )
-      )
-    );
-    this.newTable.relationships.push(
-      new Relationship(
-        this.relationship.referencing,
-        this.relationship.referenced.map((column) =>
-          column.applySourceMapping(sourceMapping)
-        )
-      )
     );
 
     // name, pk
