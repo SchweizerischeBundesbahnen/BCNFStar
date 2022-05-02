25d56bfb7a38264413d75a93fe6880d9c194f214
rothaarlappen <37540536+rothaarlappen@users.noreply.github.com>
2022-05-02 08:24:01 +0200

    enables checking multi-column-inds

--- frontend/src/app/components/check-ind/check-ind.component.ts
+++ frontend/src/app/components/check-ind/check-ind.component.ts
@@ -5,6 +5,8 @@ import { SbbTable, SbbTableDataSource } from '@sbb-esta/angular/table';
 import ITablePage from '@server/definitions/ITablePage';
 import { DatabaseService } from '@/src/app/database.service';
 import Relationship from '@/src/model/schema/Relationship';
+import { SbbDialog } from '@sbb-esta/angular/dialog';
+import { ViolatingRowsViewIndsComponent } from '../violating-rows-view-inds/violating-rows-view-inds.component';
 
 @Component({
   selector: 'app-check-ind',
@@ -29,18 +31,61 @@ export class CheckIndComponent {
   public _dataSource = new SbbTableDataSource<Record<string, any>>([]);
   public tableColumns: Array<string> = [];
 
-  constructor(public dataService: DatabaseService) {}
+  constructor(public dataService: DatabaseService, public dialog: SbbDialog) {}
 
   public addColumnRelation(): void {
-    return;
+    this.relationship._referencing.push(this.referencingColumn!);
+    this.relationship._referenced.push(this.referencedColumn!);
   }
+
   public canAddColumnRelation(): boolean {
-    return true;
+    if (
+      this.referencingColumn == undefined ||
+      this.referencedColumn == undefined
+    )
+      return false;
+    return !(
+      this.relationship._referencing.includes(this.referencingColumn!) ||
+      this.relationship._referenced.includes(this.referencedColumn)
+    );
   }
+
+  public async checkInd(): Promise<void> {
+    console.log(this.relationship);
+    const rowCount: number =
+      await this.dataService.loadViolatingRowsForINDCount(this.relationship);
+
+    if (rowCount == 0) {
+      // valid Inclusion Dependency
+    } else {
+      this.dialog.open(ViolatingRowsViewIndsComponent, {
+        data: {
+          relationship: this.relationship,
+          dataService: this.dataService,
+          rowCount: rowCount,
+        },
+      });
+    }
+  }
+
   public tableSelected(): boolean {
-    return true;
+    return this.referencedTable != undefined;
   }
   public validTables(): Array<Table> {
     return [...this.tables];
+  }
+
+  public canCheckIND(): boolean {
+    return true;
+  }
+
+  public removeColumnRelation(): void {
+    this.relationship._referenced = [];
+    this.relationship._referencing = [];
+  }
+
+  public relationshipString(): string {
+    if (this.relationship == undefined) return '';
+    return this.relationship.toString();
   }
 }
