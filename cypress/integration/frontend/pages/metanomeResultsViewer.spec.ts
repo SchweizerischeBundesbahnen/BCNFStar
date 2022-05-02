919f37736ed5d770002505dcc79abcbfb42f2995
Marie Fischer <marie.fischer@student.hpi.de>
2022-05-02 19:07:45 +0200

    add sorting by creation date to metanome results

--- cypress/integration/frontend/pages/metanomeResultsViewer.spec.ts
+++ cypress/integration/frontend/pages/metanomeResultsViewer.spec.ts
@@ -5,11 +5,10 @@ describe("The metanoem results page", () => {
     cy.visitFrontend();
     cy.contains("public").click();
     cy.contains("nation_region_denormalized").click();
-    // cy.contains("denormalized_data").click();
-    // cy.contains("customer_orders_lineitem_denormalized").click();
     cy.contains("part_partsupp_supplier_denormalized").click();
     cy.contains("Go").click();
   });
+
   beforeEach(() => {
     cy.visit(Cypress.env("FRONTEND_BASEURL") + "/#/metanome-results");
   });
@@ -18,11 +17,23 @@ describe("The metanoem results page", () => {
     cy.get("h1").contains("Metanome results");
     cy.get("table").contains("File name");
   });
+
   it("contains metanome result rows", () => {
     cy.get("tr").should("have.length.at.least", 3);
     cy.contains("de.metanome.algorithms.normalize.Normi");
     cy.contains("de.metanome.algorithms.binder.BINDERFile");
     cy.contains("public.part_partsupp_supplier_denormalized");
+  });
+
+  it("sort metanome results descending by creation date", () => {
+    cy.get(".creation-date").then((dateObjects) => {
+      let dates = Cypress._.map(dateObjects, (date) =>
+        new Date(date.innerHTML).getTime()
+      );
+      for (let i = 0; i < dates.length - 1; i++) {
+        expect(dates[i] >= dates[i + 1]).to.be.true;
+      }
+    });
   });
 
   it("delete metanome results", () => {
--- frontend/src/app/pages/metanome-results-viewer/metanome-results-viewer.component.ts
+++ frontend/src/app/pages/metanome-results-viewer/metanome-results-viewer.component.ts
@@ -24,9 +24,11 @@ export class MetanomeResultsViewerComponent {
   }
 
   public async reload() {
-    this.tableData = await firstValueFrom(
-      this.http.get<IIndexFileEntry[]>(this.url)
-    );
+    this.tableData = (
+      await firstValueFrom(this.http.get<IIndexFileEntry[]>(this.url))
+    ).sort(function (a: IIndexFileEntry, b: IIndexFileEntry) {
+      return a.createDate >= b.createDate ? -1 : 1;
+    });
   }
 
   async deleteEntry(entry: IIndexFileEntry) {
