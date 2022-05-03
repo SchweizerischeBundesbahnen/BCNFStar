30a5f39e87154d46604f29563d0e7a84930f5ff4
23mafi <68427675+23mafi@users.noreply.github.com>
2022-05-03 12:03:40 +0000

    add sorting by creation date to metanome results (#218)
    * add sorting by creation date to metanome results
    
    * fix typo and cleanup one test
    
    Co-authored-by: Paul Sieben <67124476+PaulVII@users.noreply.github.com>

--- cypress/integration/frontend/pages/metanomeResultsViewer.spec.ts
+++ cypress/integration/frontend/pages/metanomeResultsViewer.spec.ts
@@ -1,15 +1,11 @@
 /// <reference types="cypress" />
 
-describe("The metanoem results page", () => {
+describe("The metanome results page", () => {
   before(() => {
     cy.visitFrontend();
-    cy.contains("public").click();
-    cy.contains("nation_region_denormalized").click();
-    // cy.contains("denormalized_data").click();
-    // cy.contains("customer_orders_lineitem_denormalized").click();
-    cy.contains("part_partsupp_supplier_denormalized").click();
-    cy.contains("Go").click();
+    cy.selectTablesAndGo();
   });
+
   beforeEach(() => {
     cy.visit(Cypress.env("FRONTEND_BASEURL") + "/#/metanome-results");
   });
@@ -18,11 +14,23 @@ describe("The metanoem results page", () => {
     cy.get("h1").contains("Metanome results");
     cy.get("table").contains("File name");
   });
+
   it("contains metanome result rows", () => {
     cy.get("tr").should("have.length.at.least", 3);
     cy.contains("de.metanome.algorithms.normalize.Normi");
     cy.contains("de.metanome.algorithms.binder.BINDERFile");
     cy.contains("public.part_partsupp_supplier_denormalized");
+  });
+
+  it("sort metanome results descending by creation date", () => {
+    cy.get(".creation-date").then((dateObjects) => {
+      let dates = Cypress._.map(dateObjects, (date) =>
+        new Date(date.innerHTML).getTime()
+      );
+      for (let i = 0; i < dates.length - 1; i++) {
+        expect(dates[i] >= dates[i + 1]).to.be.true;
+      }
+    });
   });
 
   it("delete metanome results", () => {
