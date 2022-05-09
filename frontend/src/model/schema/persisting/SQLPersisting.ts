665918a397b974de0a00893f0aea389b40777ff3
rothaarlappen <37540536+rothaarlappen@users.noreply.github.com>
2022-05-09 16:03:11 +0200

    using tableIdentifier instead of fullName

--- frontend/src/model/schema/persisting/SQLPersisting.ts
+++ frontend/src/model/schema/persisting/SQLPersisting.ts
@@ -55,12 +55,12 @@ ALTER TABLE ${this.tableIdentifier(
   }
 
   public foreignKeySql(fk: TableRelationship): string {
-    return `ALTER TABLE ${fk.referencing.fullName} 
+    return `ALTER TABLE ${this.tableIdentifier(fk.referencing)} 
       ADD CONSTRAINT fk_${Math.random().toString(16).slice(2)}
       FOREIGN KEY (${this.generateColumnString(fk.relationship.referencing)})
-      REFERENCES ${fk.referenced.fullName} (${this.generateColumnString(
-      fk.relationship.referenced
-    )});`;
+      REFERENCES ${this.tableIdentifier(
+        fk.referenced
+      )} (${this.generateColumnString(fk.relationship.referenced)});`;
   }
 
   public primaryKeys(tables: Table[]): string {
