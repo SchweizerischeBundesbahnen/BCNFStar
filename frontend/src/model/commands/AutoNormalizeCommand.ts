35e70a4a23125a2677dfe4dc04ff1bd21b17c93e
j-adamcic <janina.adamcic@gmail.com>
2022-05-11 11:00:02 +0200

    fix auto normalize

--- frontend/src/model/commands/AutoNormalizeCommand.ts
+++ frontend/src/model/commands/AutoNormalizeCommand.ts
@@ -13,7 +13,6 @@ export default class AutoNormalizeCommand extends Command {
 
   protected override _do(): void {
     this.resultingTables = this.schema.autoNormalize(...this.tables);
-    this._redo();
   }
 
   protected override _undo(): void {
