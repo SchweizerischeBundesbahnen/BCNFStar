aff7e27c227b02746fcdd9db42fac078441d85c1
rothaarlappen <37540536+rothaarlappen@users.noreply.github.com>
2022-06-13 14:24:22 +0200

    make tests independent of each other

--- cypress/integration/server/checkUnionedKeys.spec.ts
+++ cypress/integration/server/checkUnionedKeys.spec.ts
@@ -1,68 +1,60 @@
-import { IRequestBodyUnionedKeys } from "./../../../server/definitions/IUnionedKeys";
-
 describe("The /unionedkeys route", () => {
   const route: string = Cypress.env("BACKEND_BASEURL") + "/unionedkeys";
-  const body: IRequestBodyUnionedKeys = {
-    key1: {
-      table_schema: "public",
-      table_name: "nation_region_denormalized",
-      attributes: ["n_nationkey", "n_name"],
-    },
-    key2: {
-      table_schema: "public",
-      table_name: "nation_region_denormalized",
-      attributes: ["n_nationkey", "r_name"],
-    },
-    unionedColumns: [
-      ["n_nationkey", "n_name"],
-      ["n_nationkey", "r_name"],
-    ],
-  };
 
   it("returns JSON", () => {
-    cy.request("post", route, body)
-      .its("headers")
-      .its("content-type")
-      .should("include", "application/json");
+    cy.fixture("unionedKeysBody.json").then((body) => {
+      cy.request("post", route, body)
+        .its("headers")
+        .its("content-type")
+        .should("include", "application/json");
+    });
   });
 
   it("returns 'allowed' correctly", () => {
-    cy.request("post", route, body).should((result) => {
-      expect(result.body).equal("allowed");
+    cy.fixture("unionedKeysBody.json").then((body) => {
+      cy.request("post", route, body).should((result) => {
+        expect(result.body).equal("allowed");
+      });
     });
   });
 
   it("returns 'allowed' correctly 2", () => {
-    body.key1 = body.key2;
-    body.unionedColumns[0] = body.unionedColumns[1];
-    cy.request("post", route, body).should((result) => {
-      expect(result.body).equal("allowed");
+    cy.fixture("unionedKeysBody.json").then((body) => {
+      body.key1 = body.key2;
+      body.unionedColumns[0] = body.unionedColumns[1];
+      cy.request("post", route, body).should((result) => {
+        expect(result.body).equal("allowed");
+      });
     });
   });
 
   it("returns 'forbidden' correctly", () => {
-    body.key1.attributes = ["n_nationkey"];
-    body.key2.attributes = ["n_regionkey"];
+    cy.fixture("unionedKeysBody.json").then((body) => {
+      body.key1.attributes = ["n_nationkey"];
+      body.key2.attributes = ["n_regionkey"];
 
-    body.unionedColumns = [
-      ["n_nationkey", "n_regionkey", "r_comment"],
-      ["n_regionkey", "n_nationkey", "r_comment"],
-    ];
-    cy.request("post", route, body).should((result) => {
-      expect(result.body).equal("forbidden");
+      body.unionedColumns = [
+        ["n_nationkey", "n_regionkey", "r_comment"],
+        ["n_regionkey", "n_nationkey", "r_comment"],
+      ];
+      cy.request("post", route, body).should((result) => {
+        expect(result.body).equal("forbidden");
+      });
     });
   });
 
   it("returns 'forbidden' on null-columns", () => {
-    body.key1.attributes = ["n_nationkey"];
-    body.key2.attributes = ["null"];
+    cy.fixture("unionedKeysBody.json").then((body) => {
+      body.key1.attributes = ["n_nationkey"];
+      body.key2.attributes = ["null"];
 
-    body.unionedColumns = [
-      ["n_nationkey", "n_regionkey", "r_comment"],
-      ["null", "n_nationkey", "r_comment"],
-    ];
-    cy.request("post", route, body).should((result) => {
-      expect(result.body).equal("forbidden");
+      body.unionedColumns = [
+        ["n_nationkey", "n_regionkey", "r_comment"],
+        ["null", "n_nationkey", "r_comment"],
+      ];
+      cy.request("post", route, body).should((result) => {
+        expect(result.body).equal("forbidden");
+      });
     });
   });
 });
--- cypress/integration/server/testTypeCasting.spec.ts
+++ cypress/integration/server/testTypeCasting.spec.ts
@@ -1,62 +1,64 @@
-import { IRequestBodyTypeCasting } from "./../../../server/definitions/TypeCasting";
-
 describe("The /typecasting route", () => {
   const route: string = Cypress.env("BACKEND_BASEURL") + "/typecasting";
 
-  const body: IRequestBodyTypeCasting = {
-    schema: "public",
-    table: "nation_region_denormalized",
-    column: "n_nationkey",
-    currentDatatype: "integer",
-    targetDatatype: "integer",
-  };
-
   it("returns JSON", () => {
-    cy.request("post", route, body)
-      .its("headers")
-      .its("content-type")
-      .should("include", "application/json");
+    cy.fixture("typeCastingBody.json").then((body) => {
+      cy.request("post", route, body)
+        .its("headers")
+        .its("content-type")
+        .should("include", "application/json");
+    });
   });
 
   it("returns 'allowed' correctly", () => {
-    cy.request("post", route, body).should((result) => {
-      expect(result.body).equal("allowed");
+    cy.fixture("typeCastingBody.json").then((body) => {
+      cy.request("post", route, body).should((result) => {
+        expect(result.body).equal("allowed");
+      });
     });
   });
 
   it("returns 'informationloss' correctly", () => {
-    body.targetDatatype = "character varying (1)";
-    cy.request("post", route, body).should((result) => {
-      expect(result.body).equal("informationloss");
+    cy.fixture("typeCastingBody.json").then((body) => {
+      body.targetDatatype = "character varying (1)";
+      cy.request("post", route, body).should((result) => {
+        expect(result.body).equal("informationloss");
+      });
     });
   });
 
   it("returns 'forbidden' correctly", () => {
-    body.targetDatatype = "datetime";
-    cy.request("post", route, body).should((result) => {
-      expect(result.body).equal("forbidden");
+    cy.fixture("typeCastingBody.json").then((body) => {
+      body.targetDatatype = "datetime";
+      cy.request("post", route, body).should((result) => {
+        expect(result.body).equal("forbidden");
+      });
     });
   });
 
   it("returns 'allowed' on null-column", () => {
-    body.column = "null";
-    body.targetDatatype = "integer";
-    cy.log(JSON.stringify(body));
-    cy.request("post", route, body).should((result) => {
-      expect(result.body).equal("allowed");
+    cy.fixture("typeCastingBody.json").then((body) => {
+      body.column = "null";
+      body.targetDatatype = "integer";
+      cy.log(JSON.stringify(body));
+      cy.request("post", route, body).should((result) => {
+        expect(result.body).equal("allowed");
+      });
     });
   });
 
   it("fails on invalid datatype", () => {
-    body.targetDatatype = "xxxxx";
-    body.currentDatatype = "xxxxx";
-    cy.request({
-      method: "post",
-      url: route,
-      body,
-      failOnStatusCode: false,
-    }).should((result) => {
-      expect(result.status).to.eq(422);
+    cy.fixture("typeCastingBody.json").then((body) => {
+      body.targetDatatype = "xxxxx";
+      body.currentDatatype = "xxxxx";
+      cy.request({
+        method: "post",
+        url: route,
+        body,
+        failOnStatusCode: false,
+      }).should((result) => {
+        expect(result.status).to.eq(422);
+      });
     });
   });
 });
