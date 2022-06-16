a298faf952ed73a91629811141725ee5d98e2b05
j-adamcic <janina.adamcic@gmail.com>
2022-06-16 15:24:16 +0200

    typo

--- frontend/src/app/components/graph/schema-graph/schema-graph.component.ts
+++ frontend/src/app/components/graph/schema-graph/schema-graph.component.ts
@@ -168,7 +168,7 @@ export class SchemaGraphComponent implements AfterContentInit {
     link: joint.shapes.standard.Link,
     fk: TableRelationship
   ) {
-    const generateButtonMrakup = (
+    const generateButtonMarkup = (
       selector: string,
       circleColor: string,
       path: string
@@ -196,7 +196,7 @@ export class SchemaGraphComponent implements AfterContentInit {
     ];
 
     let removeButton = new joint.linkTools.Button({
-      markup: generateButtonMrakup(
+      markup: generateButtonMarkup(
         'delete-fk-button',
         '#ff1d00',
         'M -3 -3 3 3 M -3 3 3 -3'
@@ -205,7 +205,7 @@ export class SchemaGraphComponent implements AfterContentInit {
       distance: '37%',
     });
     let joinButton = new joint.linkTools.Button({
-      markup: generateButtonMrakup(
+      markup: generateButtonMarkup(
         'join-button',
         '#2d327d',
         'M 0.715 2.327 L 0.715 -3.9 L 1.95 -3.9 L 1.95 2.197 A 6.396 6.396 90 0 1 1.9201 2.834 Q 1.8759 3.2721 1.768 3.627 A 3.1213 3.1213 90 0 1 1.5704 4.1249 A 2.2802 2.2802 90 0 1 1.248 4.6085 Q 0.91 4.992 0.442 5.174 A 2.6767 2.6767 90 0 1 -0.2509 5.3378 A 3.3124 3.3124 90 0 1 -0.598 5.356 Q -1.196 5.356 -1.7225 5.148 A 2.9679 2.9679 90 0 1 -2.2321 4.8906 A 2.3634 2.3634 90 0 1 -2.6 4.602 L -2.002 3.653 A 2.1918 2.1918 90 0 0 -1.4729 4.0586 A 2.5259 2.5259 90 0 0 -1.378 4.108 Q -1.014 4.29 -0.637 4.29 A 1.4196 1.4196 90 0 0 -0.1898 4.2224 A 1.1609 1.1609 90 0 0 0.351 3.848 A 1.2519 1.2519 90 0 0 0.5512 3.4892 Q 0.715 3.0524 0.715 2.327 Z'
