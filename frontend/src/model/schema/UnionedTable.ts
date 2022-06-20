b6d028a7d4ff5fff30524235f076d9411ea1eddc
Tobias Sträubig <tobi_2001@hotmail.de>
2022-06-20 17:10:46 +0200

    added rPriority

--- frontend/src/model/schema/UnionedTable.ts
+++ frontend/src/model/schema/UnionedTable.ts
@@ -5,6 +5,8 @@ import Table from './Table';
 export default class UnionedTable extends BasicTable {
   public tables: Array<Table>;
   public columns = new Array<Array<Column | null>>();
+  /** states for each column whether columns from table1 (false) or table2 (true) should be prioritized. */
+  public rPriority = new Array<boolean>();
 
   public constructor(
     table1: Table,
@@ -19,8 +21,11 @@ export default class UnionedTable extends BasicTable {
 
   public displayedColumns(): Array<Column> {
     return this.columns[0].map((col, i) => {
-      if (col != null) return col;
-      if (this.columns[1][i] != null) return this.columns[1][i];
+      const prefIndex = this.rPriority ? 1 : 0;
+      if (this.columns[prefIndex][i] != null)
+        return this.columns[1 - prefIndex][i];
+      if (this.columns[1 - prefIndex][i] != null)
+        return this.columns[1 - prefIndex][i];
       throw Error;
     }) as Array<Column>;
   }
