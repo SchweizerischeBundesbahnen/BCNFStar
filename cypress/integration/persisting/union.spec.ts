74d97e5fb2489ef15d92aa87931dfa2177bb6492
rothaarlappen <37540536+rothaarlappen@users.noreply.github.com>
2022-06-28 09:36:33 +0200

    integrationtests for union

--- cypress/integration/persisting/union.spec.ts
+++ cypress/integration/persisting/union.spec.ts
@@ -4,25 +4,82 @@ describe("The persist-button should create executable SQL, that UNIONs two table
   const unionedSchema = "test_schema_unioned";
   let columns: Array<string> = [];
 
-  it.only("creates executable SQL", () => {
+  beforeEach(() => {
     cy.visitFrontend();
     cy.selectSpecificTablesAndGo("public", [
       "nation_region_denormalized",
       "part_partsupp_supplier_denormalized",
     ]);
+
+    cy.loadMetanomeConfigAndOk();
+  });
+
+  it("creates unioned table named after first selected Table", () => {
     cy.unionTables(
       "public.nation_region_denormalized",
       "public.part_partsupp_supplier_denormalized",
-      []
+      [
+        ["n_nationkey", "ps_partkey"],
+        ["n_name", "p_brand"],
+        ["n_regionkey", "ps_suppkey"],
+      ]
     );
+    cy.get(".table-head-title").should("have.length", 1);
+    cy.get(".table-head-title").contains("public.nation_region_denormalized");
   });
 
-  it("union table with itself results in same rowcount", () => {});
+  it("creates executable SQL", () => {
+    cy.unionTables(
+      "public.nation_region_denormalized",
+      "public.part_partsupp_supplier_denormalized",
+      [
+        ["n_nationkey", "ps_partkey"],
+        ["n_name", "p_mfgr"],
+        ["n_regionkey", "ps_suppkey"],
+      ]
+    );
+    cy.createSchema(unionedSchema);
 
-  it("allows table union with different datatypes", () => {});
+    cy.visitFrontend();
+    cy.selectSpecificTablesAndGo(unionedSchema, ["nation_region_denormalized"]);
+  });
+
+  it("null columns work", () => {
+    cy.unionTables(
+      "public.nation_region_denormalized",
+      "public.part_partsupp_supplier_denormalized",
+      [
+        ["n_nationkey", "ps_partkey"],
+        ["n_name", "Fill Column with NULL"],
+        ["Fill Column with NULL", "ps_suppkey"],
+      ]
+    );
+    cy.createSchema(unionedSchema);
+
+    cy.visitFrontend();
+    cy.selectSpecificTablesAndGo(unionedSchema, ["nation_region_denormalized"]);
+  });
+
+  it.only("allows using same column multiple times", () => {
+    cy.unionTables(
+      "public.nation_region_denormalized",
+      "public.part_partsupp_supplier_denormalized",
+      [
+        ["n_nationkey", "ps_partkey"],
+        ["n_name", "p_mfgr"],
+        ["n_regionkey", "ps_partkey"],
+      ]
+    );
+    cy.createSchema(unionedSchema);
+
+    cy.visitFrontend();
+    cy.selectSpecificTablesAndGo(unionedSchema, ["nation_region_denormalized"]);
+  });
 
   // TODO maybe we should think about transactions?
-  after("database cleanup", () => {
+  afterEach("database cleanup", () => {
+    const downloadsFolder = Cypress.config("downloadsFolder");
+    cy.task("deleteFolder", downloadsFolder);
     cy.executeSql(`DROP SCHEMA IF EXISTS ${unionedSchema} CASCADE`);
   });
 });
