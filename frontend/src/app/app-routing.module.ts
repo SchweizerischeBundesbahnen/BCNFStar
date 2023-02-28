c331871dde33b2090803aacdc7fdaaa1f7273bf3
Marie Fischer <marie.fischer@student.hpi.de>
2023-02-28 17:33:54 +0100

    reset editing state after before starting new integration or schema editing

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,9 +1,12 @@
 import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { NavigationEnd, Router, RouterModule, Routes } from '@angular/router';
 import { SchemaEditingComponent } from './pages/schema-editing/schema-editing.component';
 import { MetanomeResultsViewerComponent } from './pages/metanome-results-viewer/metanome-results-viewer.component';
 import { HomeComponent } from './pages/home/home.component';
 import { IntegrationStartComponent } from './components/integration/integration-start/integration-start.component';
+import { SchemaService } from './schema.service';
+import { IntegrationService } from './integration.service';
+import { SchemaMergingService } from './schema-merging.service';
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
@@ -16,4 +19,22 @@ const routes: Routes = [
   imports: [RouterModule.forRoot(routes, { useHash: true })],
   exports: [RouterModule],
 })
-export class AppRoutingModule {}
+export class AppRoutingModule {
+  constructor(
+    private schemaService: SchemaService,
+    private intService: IntegrationService,
+    private mergeService: SchemaMergingService,
+    private router: Router
+  ) {
+    // reset edited schema and integration when going to home or integration page
+    this.router.events.subscribe((evt) => {
+      if (evt instanceof NavigationEnd) {
+        if (evt.url === '/' || evt.url === '/integration') {
+          this.mergeService.cancel();
+          this.intService.stopIntegration();
+          this.schemaService.hasSchema = false;
+        }
+      }
+    });
+  }
+}
--- frontend/src/app/pages/home/home.component.ts
+++ frontend/src/app/pages/home/home.component.ts
@@ -2,6 +2,7 @@ import Schema from '@/src/model/schema/Schema';
 import { Component } from '@angular/core';
 import { NavigationEnd, Router } from '@angular/router';
 import { IntegrationService } from '../../integration.service';
+import { SchemaMergingService } from '../../schema-merging.service';
 import { SchemaService } from '../../schema.service';
 
 @Component({
@@ -12,19 +13,8 @@ import { SchemaService } from '../../schema.service';
 export class HomeComponent {
   constructor(
     private schemaService: SchemaService,
-    private intService: IntegrationService,
     private router: Router
-  ) {
-    // reset edited schema and integration when going to home page
-    this.router.events.subscribe((evt) => {
-      if (evt instanceof NavigationEnd) {
-        if (evt.url === '') {
-          this.intService.stopIntegration();
-          this.schemaService.hasSchema = false;
-        }
-      }
-    });
-  }
+  ) {}
 
   public setSchemaAndGo(schema: Schema) {
     this.schemaService.schema = schema;
