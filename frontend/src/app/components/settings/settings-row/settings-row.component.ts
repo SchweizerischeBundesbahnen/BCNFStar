a769cb7b0717a7eafed220fce0406573debe6668
Marie Fischer <marie.fischer@student.hpi.de>
2023-03-02 13:42:50 +0100

    spelling and spacing things

--- frontend/src/app/components/settings/settings-row/settings-row.component.ts
+++ frontend/src/app/components/settings/settings-row/settings-row.component.ts
@@ -37,8 +37,8 @@ export class SettingsRowComponent implements OnInit, OnChanges {
   public formGroup: FormGroup = new FormGroup({});
 
   public readonly algoTooltips: Record<string, string> = {
-    HyFD: 'Fastest Metanome algorithm without approximations to find FDs',
-    BINDER: 'Slower than FAIDA, but guaranteed to produce correct results',
+    HyFD: 'Fastest Metanome algorithm without approximations to find FDs.',
+    BINDER: 'Slower than FAIDA, but guaranteed to produce correct results.',
     FAIDA: 'Recommended. Very fast, but approximate. Never produces false negatives, but might produce some false positives on small datasets.'
   }
 
