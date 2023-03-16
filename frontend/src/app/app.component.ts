d18c2e89225f7eece96980567165b2c9a8efe419
Paul Sieben <67124476+PaulVII@users.noreply.github.com>
2023-03-16 14:12:15 +0100

    add warning message if using spark

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { DatabaseService } from './database.service';
 import { isDevMode } from '@angular/core';
+import { SbbNotificationToast } from '@sbb-esta/angular/notification-toast';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -10,8 +11,13 @@ export class AppComponent {
   title = 'bcnfstar';
   public queueUrl: string;
   public devMode = isDevMode();
+  http: any;
 
-  constructor(dataService: DatabaseService) {
+  constructor(dataService: DatabaseService, notificationToast: SbbNotificationToast) {
     this.queueUrl = dataService.baseUrl + '/queue';
+    dataService.getDmbsName().then((dbmsName) => {
+      if (dbmsName == 'hive2')
+        notificationToast.open('Spark hive2 support is experimental. Note that it does not support keys', {type: 'warn'});
+    })
   }
 }
